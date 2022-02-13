b10afa12b8b11dab3c309e3e325490666cdec4d6
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-02-13 12:31:26 +0000

    restructuring template project for my purposes

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+  return (
+          <div className="application">Application</div>
+  );
+  }
+}
+
+export default App;

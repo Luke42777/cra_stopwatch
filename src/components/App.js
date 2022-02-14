acd1f19278cc3f4bfe105d59086f3d6d8ebdcf18
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-02-14 14:29:14 +0000

    working stopwatch

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
+import React, { Component } from "react";
+import "./App.css";
+import SwitchButton from "./SwitchButton";
 
-import './App.css';
-import React from 'react';
 
-class App extends React.Component {
-  render(){
-  return (
-          <div className="application">Application</div>
-  );
+class App extends Component {
+  state = { 
+    time: 0,
+    active: false
+   } 
+  intervalIndex = "";
+  handleClick = () => {
+     if(this.state.active){
+        clearInterval(this.intervalIndex);
+     }
+     else{
+        this.intervalIndex = setInterval(() => this.addSeconds(),1000)
+     }
+     this.setState({active: !this.state.active})
+   }
+   addSeconds = () => {
+     this.setState({time: this.state.time + 1})
+   }
+  render() { 
+    return (
+      <>
+      <p>{this.state.time}</p>
+      <SwitchButton click={this.handleClick} active={this.state.active} />
+      </>
+    );
   }
 }
-
+ 
 export default App;
---
+++ src/components/SwitchButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const SwitchButton = (props) => {
+    return(
+        <button onClick={props.click}>{props.active ? "stop" : "start"}</button>
+    )
+}
+
+export default SwitchButton;
